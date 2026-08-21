9f1ccbf36b82acf3850fe7329ed6ba54ed22cc62
garrettPerry <perry3004@hotmail.com>
2026-08-20 22:23:12 -0400

    bug fixes/prep for publish

--- src/Components/PhotosPage/PhotoContainer/PhotoContent.tsx
+++ src/Components/PhotosPage/PhotoContainer/PhotoContent.tsx
@@ -1,47 +1,30 @@
-import { Link } from 'react-router-dom';
 import './PhotoContent.css';
-import NavBar from '../../HomePage/NavBar/NavBar';
 
 function PhotoContent() {
     return (
         <>
-            <header><NavBar /></header>
             <div className="photo-content">
-
                 <div className="photo-gallery">
                     <div className="column">
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/candle.jpg" alt=""/>
-                        </div>
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/Alaska1.jpg" alt=""/>
-                        </div>
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/mural.jpg" alt=""/>
-                        </div>
-                        <div ></div>
+                        <img src="src/Assets/PhotosPage/candle.jpg" alt="Candle burning" className='photo'/>
+                        <img src="src/Assets/PhotosPage/Alaska1.jpg" alt="Alaskan Mountains"/>
+                        <img src="src/Assets/PhotosPage/mural.jpg" alt="Mural from Notre Dam"/>
+                        <img src='src/Assets/PhotosPage/miotSunset.jpg' alt="Grey Mazda MX5 in the sunset"/>
+                        <img src='src/Assets/PhotosPage/costaRica.jpg' alt="Costa Rican cove"/>
                     </div>
                     <div className="column">
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/savannahChurch.jpg" alt=""/>
-                        </div>
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/tobeFisheye2.jpg" alt=""/>
-                        </div>
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/tcRainbow.jpg" alt=""/>
-                        </div>
+                        <img src="src/Assets/PhotosPage/savannahChurch.jpg" alt="Church in Savannah Georgia "/>
+                        <img src="src/Assets/PhotosPage/tobeFisheye2.jpg" alt="Fisheye closeup of my dachshund"/>
+                        <img src="src/Assets/PhotosPage/tcRainbow.jpg" alt="08 Scion tC under a rainbow"/>
+                        <img src="src/Assets/PhotosPage/chanelIsland2.jpg" alt="California Chanel Islands"/>
+                        <img src="src/Assets/PhotosPage/grPrius.jpg" alt="2nd gen Toyota GR86 with a prius front bumper"/>
                     </div>
                     <div className="column">
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/tobeSneak.jpg" alt=""/>
-                        </div>
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/helen.jpg" alt=""/>
-                        </div>
-                        <div className="photo">
-                            <img src="src/Assets/PhotosPage/lizard.jpg" alt=""/>
-                        </div>
+                        <img src="src/Assets/PhotosPage/tobeSneak.jpg" alt="dachshund hiding under the bed"/>
+                        <img src="src/Assets/PhotosPage/helen.jpg" alt="river flowing through Helen Georgia"/>
+                        <img src="src/Assets/PhotosPage/lizard.jpg" alt="Leopard gecko sitting in his food bowl"/>
+                        <img src='src/Assets/PhotosPage/canyon.jpg' alt="painted desert"/>
+                        <img src='src/Assets/PhotosPage/bonsai.jpg' alt='My bonsai tree, a jade plant'/>
                     </div>
                 </div>
             </div>
